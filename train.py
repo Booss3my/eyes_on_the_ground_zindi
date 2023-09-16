a794cc3489ebcaa72cc7107b902688c73dcdf985
Bastamy Oussama <oussama.bastamy@bordeaux-inp.fr>
2023-09-17 01:37:24 +0200

    dataloaders

--- train.py
+++ train.py
@@ -6,11 +6,19 @@ from dataset.data import eog_Dataset
 import matplotlib.pyplot as plt
 from PIL import Image
 from torch.utils.data import DataLoader
-train_data  = pd.read_csv(os.path.join(DATA_ROOT_PATH,"train.csv"))
-image_paths = [os.path.join(IMAGE_PATH,filename) for filename in train_data.filename]
+from sklearn.model_selection import train_test_split
 
-dataset = eog_Dataset(image_paths, labels = train_data.extent,tfs=TRAIN_TFS)
+data  = pd.read_csv(os.path.join(DATA_ROOT_PATH,"train.csv"))
 
-DataLoader(dataset,batch_size=BATCH_SIZE,shuffle=True,num_workers=NUM_DL_WORKERS)
-display_image_tensor(dataset.__getitem__(100)[0])
+train_im, val_im, train_lab, val_lab =train_test_split(data.filename,data.extent,test_size=0.33,random_state=10,shuffle=True)
+
+train_image_paths = [os.path.join(IMAGE_PATH,filename) for filename in train_im]
+val_image_paths =  [os.path.join(IMAGE_PATH,filename) for filename in val_im]
+
+train_dataset = eog_Dataset(train_image_paths, labels = train_lab.values,tfs=TRAIN_TFS)
+val_dataset = eog_Dataset(val_image_paths, labels = val_lab.values,tfs=TRAIN_TFS)
+
+train_dataloader = DataLoader(train_dataset,batch_size=BATCH_SIZE,shuffle=True,num_workers=NUM_DL_WORKERS)
+val_dataloader = DataLoader(val_dataset,batch_size=BATCH_SIZE,shuffle=True,num_workers=NUM_DL_WORKERS)
+
 
