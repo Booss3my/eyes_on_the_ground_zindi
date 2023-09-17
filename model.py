b4b0ef1f0d38a3f87a5b8940b91684ae16eaaefd
Bastamy Oussama <oussama.bastamy@bordeaux-inp.fr>
2023-09-17 18:36:01 +0200

    test

--- model.py
+++ model.py
@@ -5,4 +5,7 @@ from config import *
 base_model= timm.create_model('efficientnet_b1_pruned', pretrained=True)
 base_model.classifier = torch.nn.Linear(in_features=1280,out_features=1,bias=True)
 base_model = torch.nn.DataParallel(base_model, device_ids=[0, 1])
+for param in base_model.parameters():
+    param.requires_grad = False
+
 base_model.to(DEVICE)
