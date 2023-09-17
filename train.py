ef9b6e72ce3f2461a0dcbfc3d83b6a6ab8edecf7
Bastamy Oussama <oussama.bastamy@bordeaux-inp.fr>
2023-09-17 18:09:15 +0200

    wandb logging

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from model import base_model
 import torch
 from torch import nn
 from tqdm import tqdm
-
+import wandb
 
 
 data  = pd.read_csv(os.path.join(DATA_ROOT_PATH,"train.csv"))
@@ -30,17 +30,21 @@ val_dataloader = DataLoader(val_dataset,batch_size=BATCH_SIZE,shuffle=True,num_w
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(lr=LR,params=base_model.parameters())
 #scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda)
-    
 
-
+wandb.login(key=WANDB_KEY)
+config = dict(learning_rate=LR, batch_size=BATCH_SIZE, epochs=NUM_EPOCHS, frac_data_used=SAMPLE_FRAC)
+wandb.init(project="detect-pedestrians", ntags=["baseline", "paper1"], config=config)
+wandb.watch(base_model, log_freq=1)
 for i in range(NUM_EPOCHS):
     running_loss = 0    
     for images,labels in tqdm(train_dataloader,f'Iterating through {len(train_dataloader)} batches'):   
         y = base_model(images.to(DEVICE)).squeeze()
         loss  = torch.sqrt(criterion(y,labels.to(DEVICE)))
         loss.backward()
-        nn.utils.clip_grad_norm_(base_model.parameters(), 1.0)
+        nn.utils.clip_grad_norm_(base_model.parameters(), 0.5)
         optimizer.step()
         optimizer.zero_grad()
         running_loss+=loss
+    
+    wandb.log({"train loss": running_loss/len(train_dataloader), "epoch": i})
     print(f'epoch {i}/{NUM_EPOCHS}: Training RMSE {running_loss/len(train_dataloader)}')
