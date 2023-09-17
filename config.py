b77276f94b42bfa284870d646e2f5d4be059bc59
Bastamy Oussama <oussama.bastamy@bordeaux-inp.fr>
2023-09-17 15:51:52 +0200

    args in config

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ parser.add_argument('--ROOT_PATH', type=str, default=r"C:\Users\oussa\OneDrive\D
 parser.add_argument('--BATCH_SIZE', type=int, default=32, help="The batch size to be used during training")
 parser.add_argument('--LR', type=float, default=1e-3, help="The learning rate to be used during training")
 parser.add_argument('--NUM_EPOCHS', type=int, default=10, help="The number of epochs to train for")
+parser.add_argument('--NUM_DL_WORKERS', type=int, default=1, help="Num workers for dataloader")
 
 args = parser.parse_args()
 
@@ -19,6 +20,7 @@ ROOT_PATH = args.ROOT_PATH
 BATCH_SIZE = args.BATCH_SIZE
 LR = args.LR
 NUM_EPOCHS = args.NUM_EPOCHS
+NUM_DL_WORKERS=args.NUM_DL_WORKERS
 
 TRAIN_TFS = A.Compose([
     A.Transpose(),
