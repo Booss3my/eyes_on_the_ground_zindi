3df5095cc543a4ff2202c6bfb259c1f41b8762f1
Bastamy Oussama <oussama.bastamy@bordeaux-inp.fr>
2023-09-18 16:19:58 +0200

    corrected model last layer setting

--- models/model.py
+++ models/model.py
@@ -6,9 +6,10 @@ from config import *
 model= timm.create_model(model_name, pretrained=True)
 model_cfg = model.default_cfg
 input_size = model_cfg["input_size"][1]
-in_feats = model.classifier.in_features
+in_feats = model.get_classifier().in_features
 
 
-model.classifier = torch.nn.Linear(in_features=in_feats,out_features=1,bias=True)
+setattr(model,model_cfg["classifier"],torch.nn.Linear(in_features=in_feats,out_features=1,bias=True))
+
 base_model = torch.nn.DataParallel(model, device_ids=[0, 1])
 base_model.to(DEVICE)
