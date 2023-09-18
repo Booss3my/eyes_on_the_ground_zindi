e0f41f57efa01538c7a37dc9d08920c4b8667795
Bastamy Oussama <oussama.bastamy@bordeaux-inp.fr>
2023-09-18 11:28:41 +0200

    ttest time augm

--- predict.py
+++ predict.py
@@ -7,25 +7,30 @@ import pandas as pd
 import os
 import torch
 
-data  = pd.read_csv(os.path.join(DATA_ROOT_PATH,"Test.csv"))
-submission_file= pd.read_csv(os.path.join(DATA_ROOT_PATH,"SampleSubmission.csv"))
-pred_fnames = submission_file.merge(data, how="inner", on="ID").filename
-
-pred_image_paths = [os.path.join(IMAGE_PATH,filename) for filename in pred_fnames]
-pred_dataset = eog_Dataset(pred_image_paths,tfs=TRAIN_TFS)
-pred_dataloader = DataLoader(pred_dataset,batch_size=BATCH_SIZE,shuffle=False,num_workers=NUM_DL_WORKERS)
-
 state_dict = torch.load(model_path)
 model.load_state_dict(state_dict)
 base_model = torch.nn.DataParallel(model, device_ids=[0, 1])
 base_model.to(DEVICE)
 
-pred_iter=iter(pred_dataloader)
-test_output=torch.tensor([]).to(DEVICE)
-for data in tqdm(pred_iter):
-    with torch.no_grad():
-        test_output = torch.cat((test_output,base_model.eval()(data.to(DEVICE))))
+
+data  = pd.read_csv(os.path.join(DATA_ROOT_PATH,"Test.csv"))
+submission_file= pd.read_csv(os.path.join(DATA_ROOT_PATH,"SampleSubmission.csv"))
+pred_fnames = submission_file.merge(data, how="inner", on="ID").filename
+
+pred_image_paths = [os.path.join(IMAGE_PATH,filename) for filename in pred_fnames]
+averaged_output=torch.tensor([]).to(DEVICE)
+for _ in range(5):
+    pred_dataset = eog_Dataset(pred_image_paths,tfs=TRAIN_TFS)
+    pred_dataloader = DataLoader(pred_dataset,batch_size=BATCH_SIZE,shuffle=False,num_workers=NUM_DL_WORKERS)
+
+    pred_iter=iter(pred_dataloader)
+    test_output=torch.tensor([]).to(DEVICE)
+    for data in tqdm(pred_iter):
+        with torch.no_grad():
+            test_output = torch.cat((test_output,base_model.eval()(data.to(DEVICE))))
+    
+    averaged_output+=test_output/5
 
 submission = submission_file.copy()        
-submission["extent"] = (100*abs(test_output)).type(torch.int).cpu()
+submission["extent"] = (100*abs(averaged_output)).type(torch.int).cpu()
 submission.to_csv("submission_ob.csv",index=False)
