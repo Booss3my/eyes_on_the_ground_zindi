db6c431a7d506c525f832c7180ca2e85154041f4
Bastamy Oussama <oussama.bastamy@bordeaux-inp.fr>
2023-10-20 11:31:52 +0200

    changed comments

--- classification_train.py
+++ classification_train.py
@@ -47,7 +47,7 @@ for i in range(NUM_EPOCHS):
     running_loss = 0    
     for j,(images,labels) in tqdm(enumerate(train_dataloader),f'Iterating through {len(train_dataloader)} batches'):   
         y = base_model(images.to(DEVICE)).squeeze()
-        loss  = torch.sqrt(criterion(y,labels.to(DEVICE)))
+        loss  = criterion(y,labels.to(DEVICE))
         (loss/N_GRAD_CUMUL).backward()
         # nn.utils.clip_grad_norm_(base_model.module.parameters(), 1.0)
         if j%N_GRAD_CUMUL==N_GRAD_CUMUL-1 or j==len(train_dataloader)-1: 
@@ -56,8 +56,8 @@ for i in range(NUM_EPOCHS):
         
         running_loss+=loss
     
-    wandb.log({"train loss": 100*running_loss/len(train_dataloader), "epoch": i})
-    print(f'epoch {i}/{NUM_EPOCHS}: Training RMSE {100*running_loss/len(train_dataloader)}')
+    wandb.log({"train loss": running_loss/len(train_dataloader), "epoch": i})
+    print(f'epoch {i}/{NUM_EPOCHS}: Training cross_entropy {running_loss/len(train_dataloader)}')
     
     #val
     if i%3==2: 
@@ -65,19 +65,19 @@ for i in range(NUM_EPOCHS):
         for images,labels in tqdm(val_dataloader,f'Iterating through {len(val_dataloader)} batches'):
             with torch.no_grad():
                  y = base_model(images.to(DEVICE)).squeeze()
-                 loss  = torch.sqrt(criterion(y,labels.to(DEVICE)))
+                 loss  = criterion(y,labels.to(DEVICE))
                  val_running_loss+=loss
         
-        lb_loss = 100*val_running_loss/len(val_dataloader) 
+        lb_loss = val_running_loss/len(val_dataloader) 
         if lb_loss<best_val_loss:
             torch.save(base_model.module.state_dict(), MODEL_SAVE_PATH)
             best_val_loss = lb_loss
         
-        if early_stopper.early_stop(100*val_running_loss/len(val_dataloader) ):             
+        if early_stopper.early_stop(val_running_loss/len(val_dataloader) ):             
             break
 
         wandb.log({"val loss": lb_loss, "epoch": i})
-        print(f'epoch {i}/{NUM_EPOCHS}: Validation RMSE {lb_loss}')
+        print(f'epoch {i}/{NUM_EPOCHS}: Validation cross entropy {lb_loss}')
 
 
     scheduler.step()
