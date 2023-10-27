2aa3d1d7fa3dc72e0d25a15cd525fa321f6beec0
Bastamy Oussama <oussama.bastamy@bordeaux-inp.fr>
2023-10-27 13:27:40 +0200

    upd

--- train_cv.py
+++ train_cv.py
@@ -36,6 +36,6 @@ for i, (train_index, val_index) in enumerate(skf.split(data.index,data.extent)):
     optimizer = Lion(base_model.module.parameters(), lr=LR, weight_decay=1e-2)
 
     loss=one_epoch(base_model, train_dataloader, criterion, optimizer,type="train")
-
+    print(loss)
 
 
