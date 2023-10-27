6dec39b29ab8963449f65fdacc8d5bb85776b656
Bastamy Oussama <oussama.bastamy@bordeaux-inp.fr>
2023-10-27 17:45:38 +0200

    update

--- train_cv.py
+++ train_cv.py
@@ -12,6 +12,7 @@ from train_one_ep import one_epoch
 seed_everything(SEED)
 
 n_splits = 5
+average_val_losses = 0
 data  = pd.read_csv(os.path.join(DATA_ROOT_PATH,"train.csv"))
 mask = data['filename'].apply(lambda x: len(x.split(" ")) <= 1)
 data = data.loc[mask].sample(frac=SAMPLE_FRAC,random_state= 10).reset_index(drop=True)
@@ -33,10 +34,7 @@ for i, (train_index, val_index) in enumerate(skf.split(data.index,data.extent)):
     
     criterion = nn.MSELoss()
     optimizer = Lion(model_parameters, lr=LR, weight_decay=1e-2)
-    average_val_losses = 0
-
-
-
+    
     print(f"######## Fold {i+1}/{n_splits}#####")
     for j in range(NUM_EPOCHS):
         train_loss = one_epoch(base_model, train_dataloader, criterion, optimizer,type="train")
@@ -46,7 +44,6 @@ for i, (train_index, val_index) in enumerate(skf.split(data.index,data.extent)):
             print(f"Epoch {j+1}/{NUM_EPOCHS} --- Validation loss :{val_loss}")
         if j+1==NUM_EPOCHS:
             average_val_losses+=val_loss
-    print(average_val_losses)
 print(f"######## Average fold validation {average_val_losses/n_splits}#####")
         
     
