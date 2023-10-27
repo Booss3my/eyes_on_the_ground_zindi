01dfee067943bce9281889a6eb4c256c2259318e
Bastamy Oussama <oussama.bastamy@bordeaux-inp.fr>
2023-10-27 13:05:27 +0200

    debug

--- train_cv.py
+++ train_cv.py
@@ -21,16 +21,16 @@ data = data.loc[mask].sample(frac=SAMPLE_FRAC,random_state= 10)
 
 skf = StratifiedKFold(n_splits=5,shuffle=True)
 for i, (train_index, val_index) in enumerate(skf.split(data.index,data.extent)):
-
+    
     train_image_paths = [os.path.join(IMAGE_PATH,data.filename[filename_idx]) for filename_idx in train_index]
     val_image_paths =  [os.path.join(IMAGE_PATH,data.filename[filename_idx]) for filename_idx in val_index]
 
-    train_dataset = EogDataset(train_image_paths, labels = data.extent[train_index],size=input_size, tfs=TRAIN_TFS)
-    val_dataset = EogDataset(val_image_paths, labels =  data.extent[val_index],size=input_size,tfs=VAL_TFS)
+    train_dataset = EogDataset(train_image_paths, labels = data.extent[train_index].values,size=input_size, tfs=TRAIN_TFS)
+    val_dataset = EogDataset(val_image_paths, labels =  data.extent[val_index].values,size=input_size,tfs=VAL_TFS)
 
     train_dataloader = DataLoader(train_dataset,batch_size=BATCH_SIZE,shuffle=True,num_workers=NUM_DL_WORKERS)
     val_dataloader = DataLoader(val_dataset,batch_size=BATCH_SIZE,shuffle=True,num_workers=NUM_DL_WORKERS)
-
+    
     criterion = nn.MSELoss()
     optimizer = Lion(model_parameters, lr=LR, weight_decay=1e-2)
 
