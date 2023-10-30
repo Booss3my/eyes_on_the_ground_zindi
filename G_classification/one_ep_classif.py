9f21a40a017609dc5230a7ebbc460cbb3da78256
Bastamy Oussama <oussama.bastamy@bordeaux-inp.fr>
2023-10-30 16:27:47 +0100

    upda

--- G_classification/one_ep_classif.py
+++ G_classification/one_ep_classif.py
@@ -14,7 +14,7 @@ def one_epoch_classif(base_model, dataloader, criterion, optimizer,type="train")
         for j,(images,labels) in tqdm(enumerate(dataloader),f'Iterating through {len(dataloader)} batches'):   
             y = base_model(images.to(DEVICE)).squeeze()
             labels =(100*labels).to(DEVICE)
-            print(labels)
+            print(y)
             loss  = criterion(y,labels)
             (loss/N_GRAD_CUMUL).backward()
 
